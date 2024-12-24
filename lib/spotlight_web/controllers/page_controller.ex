9eaf264e15f928a013d98840fa804ae459f26a7f
Stefan Pankov <stef.pankov@gmail.com>
2024-12-24 14:29:13 +0100

    initial commit

---
+++ lib/spotlight_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule SpotlightWeb.PageController do
+  use SpotlightWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
